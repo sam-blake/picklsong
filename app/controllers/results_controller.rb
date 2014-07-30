fbd29a9ce2be26865ae61da774ea9f32c988c15c
ericodes <erica.cheung@flatironschool.com>
2014-07-29 23:10:51 -0400

    Cleaning up results controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,8 +5,6 @@ class ResultsController < ApplicationController
   def create
     @query = SongPicklr::Search.new(params[:q])
     @results = @query.query_items
-    # @main = @results.first
-    @alternatives = @results
     @playlists = Playlist.all
     respond_to do |format|
       format.html {redirect_to root_path}
