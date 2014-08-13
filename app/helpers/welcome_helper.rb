39f26208d2e91d685d55dfce20af7c9fcb76ab7a
ericodes <erica.cheung@flatironschool.com>
2014-08-13 18:58:48 -0400

    Styling playlist name field

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,4 +1,7 @@
 module WelcomeHelper
+  # Devise forms call helpers which are specified by Devise.
+  # These helpers aren't present when you access a Devise form from a non-Devise controller, so we need to add them.
+
   def resource_name
     :user
   end
