502655645d604f21c9711d8515a88bacfb5f55c4
ericodes <erica.cheung@flatironschool.com>
2014-08-14 11:59:32 -0400

    Playlist name editing looks gooooodd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def randomized_background_image
+    # images = ["assets/foo.jpg", "assets/random.jpg", "assets/super_random"]
+    images = [image_url("crowd-background.jpg"), image_url("carefree-background.jpg")]
+    images[rand(images.size)]
+  end
 end
